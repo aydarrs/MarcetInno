b1840ef0509c385646eab9f2fe04313bc32f5875
AlekseyAndrianov <43870626+AlekseyAndrianov@users.noreply.github.com>
2020-07-15 12:55:50 +0300

    1091: remove templates

--- src/main/java/ru/innopolis/demo/Application.java
+++ src/main/java/ru/innopolis/demo/Application.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
-public class DemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
