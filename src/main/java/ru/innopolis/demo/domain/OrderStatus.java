36de85ace2168939fda3bb5253c32edad529fb9f
AlekseyAndrianov <43870626+AlekseyAndrianov@users.noreply.github.com>
2020-07-25 13:56:28 +0300

    1133: init

--- src/main/java/ru/innopolis/demo/domain/OrderStatus.java
+++ src/main/java/ru/innopolis/demo/domain/OrderStatus.java
@@ -1,23 +1,11 @@
 package ru.innopolis.demo.domain;
 
 public enum OrderStatus {
-    //TODO: Добавить статусы заказа (убрать лишние, оставиьт только те,
-    // что нужны на конкретном этапе проекта
 
-    //TODO: ДОбавить аннотации и комментарии
-
-    NEW("новый"),
-    QUESTION_TO_STORE("вопрос магазину"),
-    QUESTION_TO_CUSTOMER("вопрос клиенту"),
-    AGREED("согласован"),
-    COME_COMPLETED("комплектуется"),
-    COMPLETED("скомплектован"),
-    COURIER("передан на доставку"),
-    EXECUTED("выполнен"),
-    ARRIVED_SORTING_POINT("прибыл на сортировочный пункт"),
-    FINISHED("выполнен"),
-    RETURN("возврат"),
-    EXCELLENT("отменен");
+    CREATED("новый"),
+    COURIER_APPOINTED("курьер назначен"),
+    DELIVERING("передан курьеру"),
+    COMPLETED("доставлен");
 
     private String title;
 
