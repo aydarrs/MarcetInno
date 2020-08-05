9c7b4ccebc7488ce9b134e741331071089017c69
iceone23 <iceone213@gmail.com>
2020-08-05 06:22:00 -0700

    Админ страница изменения пользователя, подгрузка роли fix

--- src/main/java/ru/innopolis/demo/controller/RegisterController.java
+++ src/main/java/ru/innopolis/demo/controller/RegisterController.java
@@ -1,13 +1,10 @@
 package ru.innopolis.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.innopolis.demo.domain.UserAccount;
